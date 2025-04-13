ed1489a037ee6e1a6c0271f3d543d79c10603a5c
Helena Borges <helenamb998@gmail.com>
2025-04-13 14:43:48 -0300

    Comitando

---
+++ PcWeb/js/atividade8.js
@@ -0,0 +1,31 @@
+const botao = document.querySelector("#executar")
+const r = document.querySelector("#resultado")
+
+function at8 () {
+    let frase = document.querySelector("#frase").value
+    let letra = frase.toLowerCase().split("")
+    let resultado = ""
+    let cont = 0
+
+    while (letra.length > cont)
+    {
+        switch (letra[cont])
+        {
+            case "t": resultado += "p"; break;
+            case "p": resultado += "t"; break;
+            case "e": resultado += "o"; break;
+            case "o": resultado += "e"; break;
+            case "n": resultado += "l"; break;
+            case "l": resultado += "n"; break;
+            case "i": resultado += "a"; break;
+            case "a": resultado += "i"; break;
+            case "s": resultado += "r"; break;
+            case "r": resultado += "s"; break;
+            default: resultado += letra[cont]; break;
+        }
+        cont++
+    }
+    r.innerHTML += resultado
+}
+
+botao.addEventListener("click", at8)
